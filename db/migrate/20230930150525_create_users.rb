9774e53b8e01a80d828877e18f75346b9ae5055e
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-11-06 14:12:44 +0300

    Update user table

--- db/migrate/20230930150525_create_users.rb
+++ db/migrate/20230930150525_create_users.rb
@@ -1,7 +1,8 @@
 class CreateUsers < ActiveRecord::Migration[7.0]
   def change
     create_table :users do |t|
-      t.string :name
+      t.string :first_name
+      t.string :last_name, null: true
       t.string :email
       t.string :role #Role can be admin, user, team_lead, task_manager, coordinator
       t.string :avatar, null: true
