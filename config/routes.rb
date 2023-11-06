62d62cebaa6c2a4e07b18b2c9f921ae2d42b0a2b
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-11-06 18:41:46 +0300

    Add home controller, view & set it as root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+  get 'home/index'
+  devise_for :users
+
+  root 'home#index'
+
   resources :views
   resources :updates
   resources :worksheets
@@ -7,5 +12,4 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
 end
