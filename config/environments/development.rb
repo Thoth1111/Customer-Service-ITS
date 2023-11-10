2699668f2c004741b2d83b76194856c617699a52
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-11-10 17:21:49 +0300

    Update env mailer configurations

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -40,17 +40,7 @@ Rails.application.configure do
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: "localhost", port: 3000}
-  
-  # config.action_mailer.delivery_method = :smtp
-  # config.action_mailer.smtp_settings = {
-  #   address: ENV["SMTP_ADDRESS"],
-  #   port: ENV["SMTP_PORT"],
-  #   domain: ENV["SMTP_DOMAIN"],
-  #   user_name: ENV["SMTP_USERNAME"],
-  #   password: ENV["SMTP_PASSWORD"],
-  #   authentication: :plain,
-  #   enable_starttls_auto: true
-  # }
+
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -8,6 +8,16 @@ Rails.application.configure do
 
   # default url options for devise
   config.action_mailer.default_url_options = { host: "", port:}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: ENV["SMTP_ADDRESS"],
+    port: ENV["SMTP_PORT"],
+    domain: ENV["SMTP_DOMAIN"],
+    user_name: ENV["SMTP_USERNAME"],
+    password: ENV["SMTP_PASSWORD"],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
