12106e9616b1659837ce2b5f4f14550fe1c8a5ad
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-11-15 16:49:00 +0300

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,17 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :timeoutable, :omniauthable,
-         :trackable, :lockable, :confirmable
+         :recoverable, :rememberable, :validatable, :timeoutable, 
+         :trackable, :lockable, :confirmable, :omniauthable, omniauth_providers: [:google_oauth2]
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token(length=20)
+      user.full_name = auth.info.name
+      user.avatar = auth.info.image
+      #Skip confirmation since Google has already confirmed the email address
+      user.skip_confirmation!
+    end
+  end
 end
