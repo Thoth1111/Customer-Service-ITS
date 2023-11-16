d427beeacdd4ffe6e1e566539a9ca5c4e0b84487
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-11-16 16:20:21 +0300

    Update omniauth_callbacks controller

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -9,15 +9,27 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def google_oauth2
     @user = User.from_omniauth(auth)
 
-    if @user.present
-      sign_out_all_scopes
-      flash[:success] = t 'devise.omniauth_callbacks.success', kind: 'Google'
-      sign_in_and_redirect @user, event: :authentication
+    if @user.persisted?
+      sign_in_and_redirect @User
     else
-      flash[:alert] =
-        t 'devise.omniauth_callbacks.failure', kind: 'Google', reason: "#{auth.info.email} is not authorized."
-      redirect_to new_user_session_path
+      session['devise.google_data'] = auth.except(:extra)
+      redirect_to new_user_registration_url, alert: @user.errors.full_messages.join("\n")
     end
+
+    # if @user.present
+    #   sign_out_all_scopes
+    #   flash[:success] = t 'devise.omniauth_callbacks.success', kind: 'Google'
+    #   sign_in_and_redirect @user, event: :authentication
+    # else
+    #   flash[:alert] =
+    #     t 'devise.omniauth_callbacks.failure', kind: 'Google', reason: "#{auth.info.email} is not authorized."
+    #   redirect_to new_user_session_path
+    # end
+  end
+
+  def failure
+    flash[:alert] = 'Google authentication failed, please register or login with email and password.'
+    redirect_to new_user_session_path
   end
 
   # More info at:
