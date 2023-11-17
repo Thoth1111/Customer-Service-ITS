d16d774831ab74597f629e7fd82e7f63a15b968d
Alwyn Androvelle <alwynsimiyu@gmail.com>
2023-11-17 10:14:59 +0300

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,17 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable, :timeoutable,
          :trackable, :lockable, :confirmable, :omniauthable, omniauth_providers: [:google_oauth2]
 
-  # def self.from_omniauth(auth)
-  #   where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-  #     user.email = auth.info.email
-  #     user.password = Devise.friendly_token(20)
-  #     user.full_name = auth.info.name
-  #     user.avatar = auth.info.image
-  #     # Skip confirmation since Google has already confirmed the email address
-  #     user.skip_confirmation!
-  #   end
-  # end
-
   def self.from_omniauth(access_token)
     data = access_token.info
     user = User.where(email: data['email']).first
@@ -25,6 +14,8 @@ class User < ApplicationRecord
       full_name: data['name'],
       avatar: data['image']
     )
+    # Skip confirmation since Google has already confirmed the email address
+    user.skip_confirmation!
     user
   end
 end
