99db746136a903948ac4d36cd065634d40b47167
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-01-18 13:48:02 +0300

    Update updates models

--- app/models/update.rb
+++ app/models/update.rb
@@ -1,2 +1,6 @@
 class Update < ApplicationRecord
+    belongs_to :task
+    has_many :task_views, as: :item, dependent: :destroy
+
+    validates :note, presence: true, length: { maximum: 350 }, message: "Note is required"
 end
