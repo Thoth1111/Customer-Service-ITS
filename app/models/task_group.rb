4ccd75de34eda794a058384ed954fb2283af46ac
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-01-18 13:47:29 +0300

    Update task_groups model

--- app/models/task_group.rb
+++ app/models/task_group.rb
@@ -1,6 +1,5 @@
 class TaskGroup < ApplicationRecord
-    belongs_to :user
-    belongs_to :squad
+    belongs_to :owner, polymorphic: true
     has_many :tasks 
 
     validates :name, presence: true, length: { maximum: 30 }, message: "Name is required and must be less than 30 characters"
--- db/migrate/20240116155837_create_task_groups.rb
+++ db/migrate/20240116155837_create_task_groups.rb
@@ -2,8 +2,7 @@ class CreateTaskGroups < ActiveRecord::Migration[7.0]
   def change
     create_table :task_groups do |t|
       t.string :name
-      t.references :user, null: false, foreign_key: true
-      t.references :squad, null: true, foreign_key: true
+      t.references :owner, null: false, polymorphic: true
 
       t.timestamps
     end
