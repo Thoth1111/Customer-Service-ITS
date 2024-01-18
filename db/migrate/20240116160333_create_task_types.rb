2c564395c2a7b1f12c23b20b3642dc5c69e7af7e
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-01-18 10:44:16 +0300

    Update task_type table migration

--- db/migrate/20240116160333_create_task_types.rb
+++ db/migrate/20240116160333_create_task_types.rb
@@ -2,12 +2,13 @@ class CreateTaskTypes < ActiveRecord::Migration[7.0]
   def change
     create_table :task_types do |t|
       t.string :name, null: false
-      t.string :order_id, null: true
-      t.string :ticket_id, null: true
-      t.datetime :alert_time, null: true
-      t.datetime :deadline, null: true
-      t.datetime :start_date, null: true
-      t.datetime :end_date, null: true
+      t.boolean :order_id, default: false
+      t.boolean :ticket_id, default: false
+      t.boolean :alert_time, default: false
+      t.boolean :deadline, default: false
+      t.boolean :start_date, default: false
+      t.boolean :end_date, default: false
+      t.boolean :is_private, default: false
       t.references :status_group, null: true, foreign_key: true
 
       t.timestamps
