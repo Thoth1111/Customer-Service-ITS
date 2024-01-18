b2a2b22c76b8ecc02633dbe6fb9dc488ad7e0403
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-01-18 10:49:57 +0300

    Update user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,11 @@
 class User < ApplicationRecord
+    has_many :memberships
+    has_many :teams, through: :memberships
+    has_many :task_groups
+    has_many :tasks
+    has_many :notifications
+    has_many :task_views
+    has_many :status_colors
+    
+    validates :name, presence: true, length: { maximum: 30 } message: "Name is required and must be less than 30 characters"
 end
