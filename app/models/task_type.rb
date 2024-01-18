503a4cd1027d1ab0f340805d86b1e4f3f47272a5
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-01-18 10:44:31 +0300

    Update task_type model

--- app/models/task_type.rb
+++ app/models/task_type.rb
@@ -1,2 +1,4 @@
 class TaskType < ApplicationRecord
+    validates: :name, presence: true, message: "Name is required"
+    validates: :name, uniqueness: true, length: { maximum: 25 }, message: "Name must be unique and less than 25 characters"
 end
