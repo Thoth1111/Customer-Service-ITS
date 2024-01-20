f54eebb70344553bd762446a89857ec12eef70e7
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-01-20 10:51:35 +0300

    Add basic application routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,17 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  resources :users, only: [:index, :show, :create, :update, :destroy] do
+    resources :tasks, only: [:index, :show, :create, :update, :destroy]
+    resources :task_groups, only: [:index, :show, :create, :update, :destroy]
+    resources :task_types, only: [:index, :show, :create, :update, :destroy]
+    resources :notifications, only: [:index, :show, :destroy]
+    resources :teams, only: [:index, :show, :create, :update, :destroy] do
+      resources :tasks, only: [:index, :show, :create, :update, :destroy]
+      resources :task_groups, only: [:index, :show, :create, :update, :destroy]
+      resources :task_types, only: [:index, :show, :create, :update, :destroy]
+    end
+  end
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "landings#index"
 end
