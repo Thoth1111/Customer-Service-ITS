88c4e44ebf6c94b3cab1b48ad0293504778bfc98
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-01-24 19:42:17 +0300

    Configure Tailwind classes

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -12,6 +12,11 @@ module.exports = {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
+      colors: {
+        platinum: '#D9D9D9',
+        p_green: '#5A9776',
+        jet: '#353535'
+      }
     },
   },
   plugins: [
