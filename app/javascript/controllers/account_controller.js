3c23f960444399854b31ba7dc5111cebe9103112
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-03-01 23:40:47 +0300

    Add Stimulus Account Controller

--- app/javascript/controllers/account_controller.js
+++ app/javascript/controllers/account_controller.js
@@ -2,6 +2,25 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="account"
 export default class extends Controller {
+  static targets = ["activeField", "hiddenField"]
   connect() {
+  }
+
+  showFields() {
+    this.hiddenFieldTargets.forEach(element => {
+      element.classList.remove('hidden');
+    });
+    this.activeFieldTargets.forEach(element => {
+      element.classList.add('hidden');
+    });
+  }
+
+  hideFields() {
+    this.hiddenFieldTargets.forEach(element => {
+      element.classList.add('hidden');
+    });
+    this.activeFieldTargets.forEach(element => {
+      element.classList.remove('hidden');
+    });
   }
 }
