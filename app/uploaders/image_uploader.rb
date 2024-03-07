77241a11a26ef99b50ca8feb76d53a4450e78085
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-03-07 23:33:19 +0300

    Create Image_uploader in uploaders

---
+++ app/uploaders/image_uploader.rb
@@ -0,0 +1,6 @@
+class ImageUploader < Shrine 
+    Attacher.validate do 
+        validate_max_size 1.megabyte, message: "is too large (max is 1 MB)"
+        validate_mime_type_inclusion ['image/jpg', 'image/jpeg', 'image/png', 'image/webp'], message: "is not a JPEG, PNG, or WEBP"
+    end
+end
