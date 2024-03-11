17e4cf1f0678fada7b5d5cea35e3c177fb81b366
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-03-11 15:36:59 +0300

    Add & configure Shrine initializer

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -8,12 +8,13 @@ Cloudinary.config(
 )
 
 Shrine.storages = {
-    cache: Shrine::Storage::Cloudinary.new(prefix: "cache"), # for direct uploads
-    store: Shrine::Storage::Cloudinary.new(prefix: "ITS_avatars"),
+    cache: Shrine::Storage::Cloudinary.new(prefix: "ITS_avatars/cache"), #temporary
+    store: Shrine::Storage::Cloudinary.new(prefix: "ITS_avatars"), #permanent
 }
 
 Shrine.plugin :activerecord # loads Active Record integration
 Shrine.plugin :cached_attachment_data # enables retaining cached file across form redisplays
+Shrine.plugin :determine_mime_type # determines the MIME type of a file from its content
 Shrine.plugin :restore_cached_data # extracts metadata for assigned cached files
 Shrine.plugin :validation_helpers # provides file validations
 Shrine.plugin :validation # provides validation helpers
