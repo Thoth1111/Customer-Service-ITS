a1ea81bfcc2c76e4b9a21fd357fcfdd00f0fb6bc
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-03-11 15:37:30 +0300

    Create avatar uploader to handle avatar validations

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -1,4 +1,4 @@
-class ImageUploader < Shrine 
+class AvatarUploader < Shrine 
     Attacher.validate do 
         validate_max_size 1.megabyte, message: "is too large (max is 1 MB)"
         validate_mime_type_inclusion ['image/jpg', 'image/jpeg', 'image/png', 'image/webp'], message: "is not a JPEG, PNG, or WEBP"
