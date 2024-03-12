f2dec1614d6f548b69177157c3b1d6edc1357df6
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-03-12 12:12:53 +0300

    Fix linter error in password validator

--- app/validators/custom_password_validator.rb
+++ app/validators/custom_password_validator.rb
@@ -1,7 +1,8 @@
 class CustomPasswordValidator < ActiveModel::EachValidator
-    def validate_each(record, attribute, value)
-        unless value =~ /\A(?=.*[A-Z])(?=.*[0-9])(?=.*[^\w\s]).{8,}\z/
-            record.errors.add(attribute, :invalid_format, message: "must contain one uppercase letter, one number and one special character")
-        end
-    end
+  def validate_each(record, attribute, value)
+    return if value =~ /\A(?=.*[A-Z])(?=.*[0-9])(?=.*[^\w\s]).{8,}\z/
+
+    record.errors.add(attribute, :invalid_format,
+                      message: 'must contain one uppercase letter, one number and one special character')
+  end
 end
