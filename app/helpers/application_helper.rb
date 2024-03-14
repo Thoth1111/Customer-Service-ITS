abb7a843bad0c4b364a001440b8af3175e6b34c6
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-03-14 12:59:15 +0300

    Fix linter error in application helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,13 @@
 module ApplicationHelper
-    def month_offset(date)
-        date.beginning_of_month.wday - 1
-    end
+  def month_offset(date)
+    date.beginning_of_month.wday - 1
+  end
 
-    def today?(date)
-        date == Date.today
-    end
+  def today?(date)
+    date == Date.today
+  end
 
-    def today_class(date)
-        "bg-amber-100" if today?(date)
-    end
+  def today_class(date)
+    'bg-amber-100' if today?(date)
+  end
 end
