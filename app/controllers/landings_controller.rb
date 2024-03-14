95b68386ec1576f22dd2873118700b1891a6e8cf
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-03-14 12:56:54 +0300

    Update landing s controller to allow for calendar implementation

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -2,6 +2,6 @@ class LandingsController < ApplicationController
   before_action :authenticate_user!, only: [:index]
   def index
     @user = current_user
-    render :index
+    @date = params[:date] ? Date.parse(params[:date]) : Date.today
   end
 end
