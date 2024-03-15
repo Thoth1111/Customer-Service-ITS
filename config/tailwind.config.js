0ec6a610594d3c8a1f8554d1ff8c0c91e9b71c50
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-03-15 23:09:30 +0300

    Update tailwind.config.js

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -14,10 +14,6 @@ module.exports = {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
       colors: {
-        'platinum': '#D9D9D9',
-        'p_green': '#5A9776',
-        'jet': '#353535',
-        'light_window': 'rgba(217, 217, 217, 0.8)',
       }
     },
   },
