dce87e3c5c825a9b28e8fad4327b3c2128b70352
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-03-16 15:13:10 +0300

    Update landings controller

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -1,7 +1,20 @@
 class LandingsController < ApplicationController
-  before_action :authenticate_user!, only: [:index]
+  before_action :authenticate_user!
+  before_action :set_user_date
+
   def calendar
+  end
+
+  def update_date
+   year = params[:year].to_i
+   @date = Date.new(year, @date.month, 1)
+   render :calendar
+  end
+
+  private
+
+  def set_user_date
     @user = current_user
-    @date = params[:date] ? Date.parse(params[:date]) : Date.today
+    @date ||= Date.today
   end
 end
