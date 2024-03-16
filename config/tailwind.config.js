72ce2a7c11d64c06fc9df2e547c9deaa5daff66b
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-03-16 15:09:45 +0300

    Restore tailwind JS configuration

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -14,6 +14,10 @@ module.exports = {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
       },
       colors: {
+        'platinum': '#D9D9D9',
+        'p_green': '#5A9776',
+        'jet': '#353535',
+        'light_window': 'rgba(217, 217, 217, 0.8)',
       }
     },
   },
