3ba4db85f9216504bfea814a8ba9369adedfbc6c
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-03-16 15:17:37 +0300

    Fix linter issues

--- app/controllers/landings_controller.rb
+++ app/controllers/landings_controller.rb
@@ -2,19 +2,18 @@ class LandingsController < ApplicationController
   before_action :authenticate_user!
   before_action :set_user_date
 
-  def calendar
-  end
+  def calendar; end
 
   def update_date
-   year = params[:year].to_i
-   @date = Date.new(year, @date.month, 1)
-   render :calendar
+    year = params[:year].to_i
+    @date = Date.new(year, @date.month, 1)
+    render :calendar
   end
 
   private
 
   def set_user_date
     @user = current_user
-    @date ||= Date.today
+    @set_user_date ||= Date.today
   end
 end
