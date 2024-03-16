2f60afd5c34fe8a643b1444b023e9dfed668db30
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-03-16 20:08:07 +0300

    Update calendar controller

--- app/javascript/controllers/calendar_controller.js
+++ app/javascript/controllers/calendar_controller.js
@@ -1,5 +1,4 @@
 import { Controller } from "@hotwired/stimulus"
-import { useClickOutside } from "stimulus-use"
 
 // Connects to data-controller="calendar"
 export default class extends Controller {
